5bcf02fd50cdbeefaa6f530073c029b0e6b6a9a6
Heath Morris <heathmorriscode@gmail.com>
2017-06-30 13:56:35 -0500

    Organizing folder structure
    - Added src and src/views, and put index.html in there
    - Moved css, js and images folders into new public folder
    - Made a notes folder and shoved the add-books.html stuff in there for now, until we find out where to store it.
    - Junked the API folder and moved the note with our Google API key into notes folder

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ var express = require('express');
 
 var app = express();
 
-
 var port = 5000;
+
+
+// Add some routes BOYEEE
+app.get('/',function(req,res){
+	res.send('Hello world!');
+});
+
+app.get('/add',function(req,res){
+	res.send('Add ya books here bro');
+});
+
+app.get('/books',function(req,res){
+	res.send('Here ya books fam');
+});
+
 app.listen(5000,function(err){
 	console.log('running server on port ' + port);
 });
